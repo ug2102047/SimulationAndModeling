54b61c20a351b4475dc00da85818af2689888a4d
Puspita Baidya <ug2102047@cse.pstu.ac.bd>
2026-04-04 23:28:59 +0600

    Add Bernoulli distribution sampling and plotting

--- ProbabilityDistributions/bernouli.py
+++ ProbabilityDistributions/bernouli.py
@@ -1,1 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Parameters
+p = 0.6
+n_samples = 1000
+
+bernoulli_samples = np.random.binomial(1, p, n_samples)
+
+print("Bernoulli First 10 Samples:", bernoulli_samples[:10])
+print("Bernoulli Empirical Mean:", np.mean(bernoulli_samples))
+print("Bernoulli Theoretical Mean:", p)
+
+# Plot Bernoulli
+plt.hist(bernoulli_samples, bins=2, edgecolor='black', rwidth=0.8)
+plt.title("Bernoulli Distribution (n=1)")
+plt.xticks([0, 1])
+plt.show()
